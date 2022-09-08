0d166e25755fc54bef7eedc94cf2b9cdf459fc66
Noibisjunior <noibisjunior22@gmail.com>
2022-09-08 10:36:55 +0100

    function for beginners

--- function.js
+++ function.js
@@ -30,27 +30,95 @@
 // }
 // loop(['hamza', 'noibi', 'joseph', 'bunmi', 'Abdulsalaam'])
 //answer
-function loops(a) {
-  a.forEach(function(a) {
-    console.log('hello' + a )
-  })
+
+// function loops(a) {
+//   a.forEach(function(a) {
+//     console.log('hello',a )
+//   })
+//   }
+
+// loops(['hamza', 'noibi', 'joseph', 'bunmi', 'Abdulsalaam'])
+
+// correction
+var friends = ['hamza', 'noibi', 'joseph', 'bunmi', 'Abdulsalaam']
+
+function greetings(arr) {
+  for(var i = 0;i<arr.length;i++){
+    console.log('hello',arr[i]);
   }
-
-loops(['hamza', 'noibi', 'joseph', 'bunmi', 'Abdulsalaam'])
-
-// Assignment2
-reverse=['hannah','ada','eleven']
-function word(word) {
-    if(word === reverse ){
-        console.log('true');
-    }else{console.log('false');}
 }
-word('hannah')
+greetings(friends)
 
 
+//alternative Assignment2
+function pallindrum(){
+ let word=prompt('enter a word')
+let reverseWord = word.split('').reverse().join('')
+if(reverseWord.toLowerCase() === word.toLowerCase()){
+  alert('This word is a pallindrum')
+}else if(reverseWord.toLowerCase() !== word.toUpperCase()){
+  alert('This is not a pallindrum')
+}
+}
+pallindrum()
+// function add(num1,num2) {
+//  console.log(num1 + num2) 
+// }
 
+// add(10,10);
 
+// function sub(num1, num2) {
+//   console.log(num1 - num2)
+// }
 
+// sub(10, 10)
+
+// function div(num1, num2) {
+//   console.log(num1 / num2)
+// }
+
+// div(10, 10)
+
+// function calc(operation,num1,num2) {
+//   if (operation === '+') {
+//     console.log(num1 + num2)
+//   } else if (operation === '-') {
+//     console.log(num1 - num2)
+//   } else if (operation === '*') {
+//     console.log(num1 * num2)
+//   } else if (operation === '/') {
+//     console.log(num1 / num2)
+//   }
+// }
+// calc('+',10,5)
+// //understanding a callback function
+// function calc(operation, num1, num2) {
+//   if (operation === '+') {
+//     add(num1 + num2,num2)//calling the add function inside the calc function
+//   } else if (operation === '-') {
+//     console.log(num1 - num2)
+//   } else if (operation === '*') {
+//     console.log(num1 * num2)
+//   } else if (operation === '/') {
+//     console.log(num1 / num2)
+//   }
+// }
+// calc()
+
+// function GPA(score,grade) {
+//   var score=prompt('enter your score humbly')
+//   if(score >= 70 && score<= 100){alert('distinction')}
+//   else if(score >= 60 && score <= 50){
+//     alert('upper credit')
+//   }
+// }
+// GPA()
+
+// function names(name,grade) {
+//   console.log(`hey ${name} your score is ${grade}`);
+  
+// }
+// names('NOIBISJUNIOR','A')
 
 
 
